55c7d50a46fdb53bd5015256144824a3184f317a
Gabriel <gluzjabar@gmail.com>
2023-12-06 19:44:28 -0300

    Integração do Quiz com SupaBase.

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-url-polyfill/auto';
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
